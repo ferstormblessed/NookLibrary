8f414f246b0e0c5be92081c24bf9eeed483c9c41
stormblessed <salvadorfernando2002@outlook.com>
2024-11-03 22:01:32 -0600

    try something with the games includes

---
+++ include/NOOK/Games/Games.h
@@ -0,0 +1,13 @@
+//
+// Created by stormblessed on 11/3/24.
+//
+
+#pragma once
+
+#ifndef NOOK_GAMES_H
+#define NOOK_GAMES_H
+
+#include "Pong.h"
+#include "InfiniteRunner.h"
+
+#endif //NOOK_GAMES_H
